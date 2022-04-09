43fd2701d1e0ff26996e2b19b3ab0c043be7ed14
varsha2040 <103292970+varsha2040@users.noreply.github.com>
2022-04-09 10:43:55 -0400

    Add files via upload

---
+++ prompts.py
@@ -0,0 +1,14 @@
+#5 prompts for each: self care, meditation, journal, exercise
+import random
+user_input= "What type of prompt would you like (self care,meditation, journal or exercise)?"
+
+r = random.randint(0,4)
+self_care_list= ["Make sure to keep yourself hydrated by drinking water!",
+                 "Rock out to some music!", "Eat a snack", "Stretch your legs!",
+                 "Take a page from a coloring book and colour!"]
+if user_input=="self care"
+for i in range(1):
+    print(self_care_list[r])
+
+
+
