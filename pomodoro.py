61c87340d15289f6323bb4eaf9d80b6cd295f1eb
Tatiana Voegerl <tatianavoegerl@gmail.com>
2022-04-09 01:57:08 -0400

    Added env and practiced Flask

--- pomodoro.py
+++ pomodoro.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return "Hello World"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug = True)
